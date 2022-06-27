56f33fa298c03f9288f062b17b260e417e5ec8ff
Charles K.S <ctmbr@protonmail.com>
2022-06-27 11:41:15 -0700

    Finished Server

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const mongodb = require("mongodb").MongoClient;
+const connect = require("./config/connection");
+// const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const connectionStringURI = `mongodb://localhost:27017/MongoNetDB`;
-
-let db;
-
-mongodb.connect(
-  connectionStringURI,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-
-  //   app.listen(port, () => {
-  //     console.log(`Example app listening at http://localhost:${port}`);
-  //   });
-  // }
-);
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(routes);
+
+connect.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
+});
